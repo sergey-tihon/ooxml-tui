71c405699d81e2694d322b2550a13dae5239da53
Sergey Tihon <sergey_tihon@epam.com>
2024-05-14 23:11:04 +0200

    hk: cleanup for custom ui

--- src/app.rs
+++ src/app.rs
@@ -1,58 +1,34 @@
-use std::collections::HashMap;
-
-pub enum CurrentScreen {
-    Main,
-    Editing,
-    Exiting,
-}
-
-pub enum CurrentlyEditing {
-    Key,
-    Value,
-}
+use std::io;
 
 pub struct App {
-    pub key_input: String,
-    pub value_input: String,
-    pub pairs: HashMap<String, String>,
-    pub current_screen: CurrentScreen,
-    pub currently_editing: Option<CurrentlyEditing>,
+    pub file_path: String,
+    pub entries: Vec<String>,
 }
 
 impl App {
-    pub fn new() -> Self {
-        Self {
-            key_input: String::new(),
-            value_input: String::new(),
-            pairs: HashMap::new(),
-            current_screen: CurrentScreen::Main,
-            currently_editing: None,
-        }
+    pub fn from_file(path: String) -> io::Result<Self> {
+        let file = std::fs::File::open(path.clone())?;
+        let archive = zip::ZipArchive::new(file)?;
+
+        Ok(Self {
+            file_path: path,
+            entries: archive.file_names().map(|name| name.to_string()).collect(),
+        })
     }
+}
 
-    pub fn save_key_value(&mut self) {
-        self.pairs
-            .insert(self.key_input.clone(), self.value_input.clone());
+#[cfg(test)]
+mod tests {
+    use crate::App;
+    use std::io;
 
-        self.key_input = String::new();
-        self.value_input = String::new();
-        self.currently_editing = None;
-    }
+    #[test]
+    fn load_pptx() -> io::Result<()> {
+        let app = App::from_file("data/sample.pptx".to_string())?;
+        app.entries.iter().for_each(|name| {
+            println!("{}", name);
+        });
 
-    pub fn toggle_editing(&mut self) {
-        if let Some(edit_mode) = &self.currently_editing {
-            match edit_mode {
-                CurrentlyEditing::Key => self.currently_editing = Some(CurrentlyEditing::Value),
-                CurrentlyEditing::Value => self.currently_editing = Some(CurrentlyEditing::Key),
-            };
-        } else {
-            self.currently_editing = Some(CurrentlyEditing::Key);
-        }
-    }
-
-    pub fn print_json(&self) -> serde_json::Result<()> {
-        let json = serde_json::to_string_pretty(&self.pairs)?;
-        println!("{}", json);
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, io};
 
+use app::App;
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
@@ -10,8 +11,6 @@ use ratatui::prelude::*;
 mod app;
 mod ui;
 
-use app::{App, CurrentScreen, CurrentlyEditing};
-
 fn main() -> Result<(), Box<dyn Error>> {
     // setup terminal
     enable_raw_mode()?;
@@ -21,8 +20,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut terminal = Terminal::new(backend)?;
 
     // createa app and run it
-    let mut app = App::new();
-    let res = run_app(&mut terminal, &mut app);
+    let mut app = App::from_file("data/sample.pptx".to_string())?;
+    run_app(&mut terminal, &mut app)?;
 
     // restore rerminal
     disable_raw_mode()?;
@@ -33,18 +32,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     )?;
     terminal.show_cursor()?;
 
-    if let Ok(do_print) = res {
-        if do_print {
-            app.print_json()?;
-        }
-    } else if let Err(err) = res {
-        println!("{err:?}");
-    }
-
     Ok(())
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<()> {
     loop {
         terminal.draw(|f| ui::ui(f, app))?;
 
@@ -52,69 +43,74 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
             if key.kind == event::KeyEventKind::Release {
                 continue;
             }
-            match app.current_screen {
-                CurrentScreen::Main => match key.code {
-                    KeyCode::Char('e') => {
-                        app.current_screen = CurrentScreen::Editing;
-                        app.currently_editing = Some(CurrentlyEditing::Key);
-                    }
-                    KeyCode::Char('q') => {
-                        app.current_screen = CurrentScreen::Exiting;
-                    }
-                    _ => {}
-                },
-                CurrentScreen::Exiting => match key.code {
-                    KeyCode::Char('y') => return Ok(true),
-                    KeyCode::Char('n') | KeyCode::Char('q') => return Ok(false),
-                    _ => {}
-                },
-                CurrentScreen::Editing if key.kind == event::KeyEventKind::Press => {
-                    match key.code {
-                        KeyCode::Enter => {
-                            if let Some(editing) = &app.currently_editing {
-                                match editing {
-                                    CurrentlyEditing::Key => {
-                                        app.currently_editing = Some(CurrentlyEditing::Value)
-                                    }
-                                    CurrentlyEditing::Value => {
-                                        app.save_key_value();
-                                        app.current_screen = CurrentScreen::Main;
-                                    }
-                                }
-                            }
-                        }
-                        KeyCode::Backspace => {
-                            if let Some(editing) = &app.currently_editing {
-                                match editing {
-                                    CurrentlyEditing::Key => {
-                                        app.key_input.pop();
-                                    }
-                                    CurrentlyEditing::Value => {
-                                        app.value_input.pop();
-                                    }
-                                }
-                            }
-                        }
-                        KeyCode::Tab => {
-                            app.toggle_editing();
-                        }
-                        KeyCode::Char(value) => {
-                            if let Some(editing) = &app.currently_editing {
-                                match editing {
-                                    CurrentlyEditing::Key => {
-                                        app.key_input.push(value);
-                                    }
-                                    CurrentlyEditing::Value => {
-                                        app.value_input.push(value);
-                                    }
-                                }
-                            }
-                        }
-                        _ => {}
-                    }
-                }
-                _ => {}
+
+            if let KeyCode::Char('q') = key.code {
+                return Ok(());
             }
+
+            // match app.current_screen {
+            //     CurrentScreen::Main => match key.code {
+            //         KeyCode::Char('e') => {
+            //             app.current_screen = CurrentScreen::Editing;
+            //             app.currently_editing = Some(CurrentlyEditing::Key);
+            //         }
+            //         KeyCode::Char('q') => {
+            //             app.current_screen = CurrentScreen::Exiting;
+            //         }
+            //         _ => {}
+            //     },
+            //     CurrentScreen::Exiting => match key.code {
+            //         KeyCode::Char('y') => return Ok(true),
+            //         KeyCode::Char('n') | KeyCode::Char('q') => return Ok(false),
+            //         _ => {}
+            //     },
+            //     CurrentScreen::Editing if key.kind == event::KeyEventKind::Press => {
+            //         match key.code {
+            //             KeyCode::Enter => {
+            //                 if let Some(editing) = &app.currently_editing {
+            //                     match editing {
+            //                         CurrentlyEditing::Key => {
+            //                             app.currently_editing = Some(CurrentlyEditing::Value)
+            //                         }
+            //                         CurrentlyEditing::Value => {
+            //                             app.save_key_value();
+            //                             app.current_screen = CurrentScreen::Main;
+            //                         }
+            //                     }
+            //                 }
+            //             }
+            //             KeyCode::Backspace => {
+            //                 if let Some(editing) = &app.currently_editing {
+            //                     match editing {
+            //                         CurrentlyEditing::Key => {
+            //                             app.key_input.pop();
+            //                         }
+            //                         CurrentlyEditing::Value => {
+            //                             app.value_input.pop();
+            //                         }
+            //                     }
+            //                 }
+            //             }
+            //             KeyCode::Tab => {
+            //                 app.toggle_editing();
+            //             }
+            //             KeyCode::Char(value) => {
+            //                 if let Some(editing) = &app.currently_editing {
+            //                     match editing {
+            //                         CurrentlyEditing::Key => {
+            //                             app.key_input.push(value);
+            //                         }
+            //                         CurrentlyEditing::Value => {
+            //                             app.value_input.push(value);
+            //                         }
+            //                     }
+            //                 }
+            //             }
+            //             _ => {}
+            //         }
+            //     }
+            //     _ => {}
+            // }
         }
     }
 }
